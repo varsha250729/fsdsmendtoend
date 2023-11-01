697492e2c8d46b89a5f43bd984e305a6ef1b0ec0
“Varsha <varsha0725@gmail.com>
2023-11-01 16:01:18 +0530

    data ingestion updated

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,5 +1,14 @@
 
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
 
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
 
 class DataIngestion:
     def __init__(self):
